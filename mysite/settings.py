03bf38b03067a60de920dac579e3846b597e25c1
Predator <eglebabachinaite123@gmail.com>
2023-07-26 14:23:49 +0300

    removed key

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rsp!$g$cfv^xw25&g!&4k7vq$2ja#8f97gh+++4y#)nyj=*p9o'
+SECRET_KEY = open('key', 'r')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
